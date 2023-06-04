77aeab9ecf22dcabdcb3d062b3509f04ea97df1b
Martachis <m.albimuro@gmail.com>
2023-06-04 18:11:28 +0200

    Milestone 3 acabado

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ Esta prueba evaluará tu comprensión de:
 - Manipulación del DOM usando JavaScript
 
 ## Directrices
- 
+
 - Los recursos online que puedes usar son:
+
   - [MDN docs](https://developer.mozilla.org/es/)
   - [JEST docs](https://jestjs.io/es-ES/docs/expect)
 
@@ -22,21 +23,21 @@ Esta prueba evaluará tu comprensión de:
 
 Ponte nota en cada pregunta. Explica las razones por las que te has dado esa nota.
 
-| Nota | Explicación                                                                  |
-| :--: | ---------------------------------------------------------------------------- |
-|  4   | Lo he entendido perfectamente y no he tenido ningún problema para responder  |
-|  3   | Creo que lo hice bien, pero no entendí del todo por qué funcionaba           |
-|  2   | Creo que he estado cerca, pero no obtuve la respuesta                        |
-|  1   | Tengo dificultades para responder esta pregunta                              |
-|  0   | No he intentado responder esta pregunta (explica por qué)                    |
+| Nota | Explicación                                                                 |
+| :--: | --------------------------------------------------------------------------- |
+|  4   | Lo he entendido perfectamente y no he tenido ningún problema para responder |
+|  3   | Creo que lo hice bien, pero no entendí del todo por qué funcionaba          |
+|  2   | Creo que he estado cerca, pero no obtuve la respuesta                       |
+|  1   | Tengo dificultades para responder esta pregunta                             |
+|  0   | No he intentado responder esta pregunta (explica por qué)                   |
 
 ### 1. Recorrer un árbol binario
 
 Completa el archivo binary_tree.js para superar todos los tests unitarios de \_binary_tree.js.
 Agrega 2 tests adicionales para esta función.
 
-- Nota:
-- Explicación:
+- Nota: 4
+- Explicación: He podido hacerlo pero he tenido que revisar documentación y anteriores ejercicios para entender que tenía que usar aún más recursividad. Sin haber mirado documentación no lo habría podido hacer de 0
 
 ### 2. Manipulación del DOM
 
--- src/dom-manipulation/index.js
+++ src/dom-manipulation/index.js
@@ -1,17 +1,49 @@
 /*
-* Usa todo lo que has aprendido para manipular el DOM usando sólo JavaScript
-* para completar las preguntas debajo cuando hagas "submit" en el formulario:
-* 1. Cambia el estilo del elemento <body> para que todo el texto tenga una font-family: "Verdana, sans-serif".
-* 2. Asegúrate que todos los elementos <span> muestren el contenido que has introducido en los inputs.
-* 3. Haz un loop que cambie la clase de cada <li> a "list-item"
-* 5. Crea un nuevo elemento <img>, haz que el atributo src sea una imagen tuya y añádelo a la parte superior
-* de la sección "Sobre mi". Si necesitas añadir algún elemento adicional al index.html, puedes hacerlo,
-* pero crédito extra si lo logras sin añadir elementos adicionales. 
-* 5. Bonus - Modifica y añade estilo al layout para que tu página "Sobre mi" tenga una aspecto más pulido.
+ * Usa todo lo que has aprendido para manipular el DOM usando sólo JavaScript
+ * para completar las preguntas debajo cuando hagas "submit" en el formulario:
+ * 1. Cambia el estilo del elemento <body> para que todo el texto tenga una font-family: "Verdana, sans-serif".
+ * 2. Asegúrate que todos los elementos <span> muestren el contenido que has introducido en los inputs.
+ * 3. Haz un loop que cambie la clase de cada <li> a "list-item"
+ * 5. Crea un nuevo elemento <img>, haz que el atributo src sea una imagen tuya y añádelo a la parte superior
+ * de la sección "Sobre mi". Si necesitas añadir algún elemento adicional al index.html, puedes hacerlo,
+ * pero crédito extra si lo logras sin añadir elementos adicionales.
+ * 5. Bonus - Modifica y añade estilo al layout para que tu página "Sobre mi" tenga una aspecto más pulido.
  */
 
-function handleSubmit() {
+const bodyEl = document.querySelector("body");
+bodyEl.style.fontFamily = "Verdana, sans-serif";
+
+const imgEl = document.createElement("img");
+imgEl.src = "../Assets/Marta_Noa.JPG";
+imgEl.style.width = "300px";
+imgEl.style.height = "auto";
+imgEl.classList.add("img-Noa-Marta");
+const divImg = document.getElementById("img-cabecera");
+divImg.appendChild(imgEl);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
   alert("Submit!");
+
+  const inputRestaurant = document.getElementById("restaurantsInput");
+  const spanRestaurant = document.getElementById("restaurant");
+  spanRestaurant.textContent = inputRestaurant.value;
+
+  const inputNeighborhood = document.getElementById("neighborhoodInput");
+  const spanNeighborhood = document.getElementById("neighborhood");
+  spanNeighborhood.textContent = inputNeighborhood.value;
+
+  const inputHometown = document.getElementById("hometownInput");
+  const spanHometown = document.getElementById("hometown");
+  spanHometown.textContent = inputHometown.value;
+
+  const liEls = document.querySelectorAll("li");
+  for (let i = 0; i < liEls.length; i++) {
+    liEls[i].classList.add("list-item");
+  }
+
+  form.reset();
 }
 
 let form = document.querySelector("form");
